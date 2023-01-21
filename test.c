defec530ce0149106aca5c48bc6067d2dc0f0626
Keimel R <keimelrodriguez12@gmail.com>
2023-01-20 22:02:26 -0430

    Correcion de error en la funcion remover() y adicion de mas codigo

--- test.c
+++ test.c
@@ -22,11 +22,15 @@ struct vehiculo
 } _vehiculo[MAX];
 
 int cont = -1;
-int first = -1;
+int cant_elementos_cola = 0;
+
+// Prototipos de fuciones
 
 void insertar();
-void cantidad_cola();
-void remover();
+
+int cantidad_cola();
+
+bool remover();
 bool cola_vacia();
 bool cola_llena();
 void elementos(); 
@@ -34,7 +38,7 @@ void elementos();
 int main(int argc, char const *argv[])
 {
 		int opcion;
-		
+
 		do
 		{
 			printf("Presione '1' para insertar un elemento a la cola\n");
@@ -48,19 +52,27 @@ int main(int argc, char const *argv[])
 			switch(opcion)
 			{
 				case 1:	insertar();
+						system("cls");
 						break;
 
-				case 2:	cantidad_cola();
+				case 2:	printf("%d elementos en cola\n", cantidad_cola());
 						break;
 
-				case 3:	remover();
-						printf("Elemento removido\n");
+				case 3:	if(remover() != false)
+						{
+							printf("Elemento removido\n");
+						}	
+						else	
+						{
+							printf("No hay elementos para remover\n");
+						}
 						break;
 
 				case 4:	elementos();
 						break;
 
-				case 5:	return 0;
+				case 5: return 0;
+						break;
 
 				default: printf("Opcion invalida, intentarlo nuevamente\n");
 						break;
@@ -72,13 +84,20 @@ int main(int argc, char const *argv[])
 		return 0;
 }
 
+/****************************************************************************
+*	insertar():	Permite insertar los elementos en formato de cola en caso	*
+*				de que cola_llena() retorne true, en caso contrario,		*
+*				desplegara una advertencia de que no se ha podido insertar	*
+*				el nuevo elemento en cola.									*
+*****************************************************************************/
+
 void insertar()
 	{
-		int type;
-
 		if(cola_llena() != true)
 		{
 			++cont;
+			++cant_elementos_cola;
+
 			printf("Placa del vehiculo:\n");
 			fgets(_vehiculo[cont].placa, 8, stdin);
 			fflush(stdin);
@@ -94,63 +113,92 @@ void insertar()
 			printf("Escoja uno de los siguientes tipos de vehiculos\n"); 
 			printf("Presione '1' para una moto\nPresione '2' para un auto\n");
 			printf("Presione '3' para una camion\nPresione '4' para un bus\n");
-			scanf("%d", &type);
-			_vehiculo[cont].c_tipo = type;
+			scanf("%d", &_vehiculo[cont].c_tipo);			
 
 			printf("Anio del vehiculo: \n");
 			scanf("%4d", &_vehiculo[cont].year);
 			fflush(stdin);
-			system("cls");
 		}
 		else
 			printf("Cola llena, no se puede insertar mas elementos\n");
 	}
 
-void cantidad_cola()
+/****************************************************************************
+*	cantidad_cola():	Retorna la cantidad de elementos en cola que posea 	*
+*					 	la struct _vehiculo[] actualmente.					*
+*****************************************************************************/
+
+int cantidad_cola()
 	{
-		printf("Hay %d elementos\n", cont);
+		return cant_elementos_cola;
 	}
+
+// Funcion temporal para verificar que se pudo borrar en formato cola la informacion.
 
 void elementos()
 	{
-		for (int i = 0; i <= cont; ++i)
-		{
-			printf("%s\n", _vehiculo[i].placa);		
-			printf("%s\n", _vehiculo[i].marca);
-			printf("%s\n", _vehiculo[i].modelo);
-			printf("%d\n", _vehiculo[i].c_tipo);
-			printf("%d\n", _vehiculo[i].year);
-			putchar('\n');
-		}
-	}
+			for (int i = 0; i <= cont; ++i)
+			{
+				printf("%s\n", _vehiculo[i].placa);		
+				printf("%s\n", _vehiculo[i].marca);
+				printf("%s\n", _vehiculo[i].modelo);
+				printf("%d\n", _vehiculo[i].c_tipo);
+				printf("%d\n", _vehiculo[i].year);
+				putchar('\n');
+			}			
+	}	
+
+/****************************************************************************
+*	cola_vacia():	En caso de no existir elementos en cola, retornara true	*
+*				  	para evitar un Underflow en la ejecucion del programa.	*
+*****************************************************************************/
 
 bool cola_vacia()
 	{
-		if(cont == -1)
+		if(cant_elementos_cola == 0)
 		{
 			return true;
 		}
 		return false;
 	}
+
+/****************************************************************************
+*	cola_llena():	En caso de sobrepasar la cantidad de elementos 			*
+*				  	permitidos en cola, retornara true para evitar un  		*
+*				  	Overflow en la ejecucion del programa. 					*
+*****************************************************************************/
 
 bool cola_llena()
 	{
-		if(cont >= MAX)
+		if(cant_elementos_cola >= MAX)
 		{
 			return true;
 		}
 		return false;
 	}
 
-void remover()
-	{
+/****************************************************************************
+*	remover():	Permite eliminar un elemento usando el metodo de  colas, 	*
+*				retornando true en caso de que se concrete exitosamente la	*
+*				accion.														*
+*****************************************************************************/
 
-		if(first == MAX)
+bool remover()
+	{
+		if(cola_vacia() == false)
 		{
-			first = 0;
+			for (int i = 0; i < cont; i++)
+			{
+				strcpy(_vehiculo[i].placa, _vehiculo[i + 1].placa);
+				strcpy(_vehiculo[i].modelo, _vehiculo[i + 1].modelo);
+				strcpy(_vehiculo[i].marca, _vehiculo[i + 1].marca);
+				_vehiculo[i].year = _vehiculo[i + 1].year;
+				_vehiculo[i].c_tipo = _vehiculo[i + 1].c_tipo;
+			}
+			--cant_elementos_cola;
+			--cont;
+			return true;
 		}
-		if(cola_vacia() != true)
-		{
-			cont--;
-		}
+		else
+			return false;
 	}
